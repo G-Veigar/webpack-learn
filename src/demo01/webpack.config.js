fefb344317a5886cddcfeb1274b857c9bb6d281f
gaoge2 <gaoge2@vipkid.com.cn>
2020-01-05 17:44:52 +0800

    save

--- src/demo01/webpack.config.js
+++ src/demo01/webpack.config.js
@@ -1,12 +1,18 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     // mode: 'production',
     mode: 'development',
-    entry: path.resolve(__dirname, './index.js'),,
+    entry: path.resolve(__dirname, './index.js'),
     devtool: 'source-map',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, '../../dist')
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: '管理输出'
+        })
+    ]
 }
--- src/demo02/webpack.config.js
+++ src/demo02/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
     // mode: 'production',
@@ -11,5 +13,11 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, '../../dist')
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../../index.html'),
+        })
+    ]
 }
